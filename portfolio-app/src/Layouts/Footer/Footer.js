a9b47f172a242188d06bbf5d15f239dfaeb4fb3b
rishcodelib <ri.sbh23@gmail.com>
2025-01-25 11:39:30 +0530

    added Footer

--- portfolio-app/src/Layouts/Footer/Footer.js
+++ portfolio-app/src/Layouts/Footer/Footer.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
-      <p>Footer content goes here</p>
+    <footer className="footer">
+      <div className="row">
+        <div className="container text-center">
+          <div className='col-sm-12'>
+            <p>&copy; {new Date().getFullYear()} Rishabh Aggarwal. All rights reserved.
+              <a href="https://www.linkedin.com/in/iamrishabhh" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none" ><i className="fa-brands fa-linkedin-in "></i></a> |
+              <a href="https://github.com/rishcodelib" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-github "></i></a>|
+              <a href="https://twitter.com/im_rishabhh" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-x-twitter "></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
