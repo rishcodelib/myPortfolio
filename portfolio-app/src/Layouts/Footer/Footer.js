35b9800fa8244b7eda44f07113c5613b6496ecd5
rishcodelib <ri.sbh23@gmail.com>
2025-01-30 22:02:24 +0530

    added footer icons

--- portfolio-app/src/Layouts/Footer/Footer.js
+++ portfolio-app/src/Layouts/Footer/Footer.js
@@ -11,11 +11,19 @@ function Footer() {
         <div className="container text-center">
           <YtChannel />
           <div className='col-md-12'>
-            <div className="text">&copy; {new Date().getFullYear()} Rishabh Aggarwal. All rights reserved.
-              <a href="https://www.linkedin.com/in/iamrishabhh" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none" ><i className="fa-brands fa-linkedin-in "></i></a> |
-              <a href="https://github.com/rishcodelib" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-github "></i></a>|
-              <a href="https://twitter.com/im_rishabhh" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-x-twitter "></i></a>
-            </div>
+
+          </div>
+          <div className='col-md-12 my-2'> |<a href="https://www.linkedin.com/in/iamrishabhh" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none" ><i className="fa-brands fa-linkedin-in "></i></a> |
+            <a href="https://github.com/rishcodelib" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-github "></i></a>|
+            <a href="https://twitter.com/im_rishabhh" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-x-twitter "></i></a>|
+            <a href="https://www.youtube.com/@rishhiiimusic" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-youtube "></i></a>|
+            <a href="http://www.instagram.com/iam.rishhiii" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-instagram "></i></a>|
+            <a href="http://www.snapchat.com/" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-snapchat "></i></a>|
+            <a href="http://www.google.com/" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-google "></i></a>|
+            <a href="http://www.gmail.com/" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-regular fa-envelope "></i></a>|
+            <a href="http://www.telegram.com/" target="_blank" rel="noopener noreferrer" className="text-dark p-2 text-decoration-none"><i className="fa-brands fa-telegram "></i></a>|
+          </div>
+          <div className="text">&copy; {new Date().getFullYear()} Rishabh Aggarwal. All rights reserved.
           </div>
         </div>
 
