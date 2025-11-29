ff1052a199df123edd8e48e3a879b140600116c7
rishcodelib <ri.sbh23@gmail.com>
2025-11-29 16:57:55 +0530

    updated navbar hide issue

--- portfolio-app/src/Components/NavbarComponent/navbar.js
+++ portfolio-app/src/Components/NavbarComponent/navbar.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import '../../Assets/fonts/Caveat/caveat.css'; // Import the custom font CSS
 
 import UserEntity from '../../Entity/userEntity';
 function Navbar() {
 
   const root = UserEntity.getUser(1);
+  const location = useLocation();
 
   const [offset, setOffset] = useState(0);
-  let fullUrl = window.location.pathname;
+  let fullUrl = location.pathname === '/';
+  // console.log("Full URL:", location.pathname);
+
   useEffect(() => {
     const onScroll = () => setOffset(window.scrollY);
     // clean up code
@@ -16,41 +19,41 @@ function Navbar() {
     window.addEventListener('scroll', onScroll, { passive: true });
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
-  let classStr = "navbar-light bg-light";
+  let classStr = fullUrl ? "navbar-dark bg-transparent fixed-top" : "navbar-light bg-light fixed-top";
 
-  if (offset > 200 && (fullUrl === '/' || fullUrl === '/home')) {
-    classStr = "navbar-light bg-light fixed-top ";
 
-  } else if (offset < 200 && (fullUrl === '/' || fullUrl === '/home')) {
-    classStr = "navbar-dark bg-transparent fixed-top ";
-  } 
-  
-  if (fullUrl === '/projects' || fullUrl === '/contact' || fullUrl === '/RishhiiiMusic' || fullUrl === '/RishhiiiTheExplorer' || fullUrl === '/travel') {
-    classStr = "navbar-light bg-light ";
+  if (fullUrl === '/' || fullUrl === '/home') {
+    if (offset < 200) {
+      classStr = "navbar-light bg-light fixed-top";
+    } else {
+      classStr = "navbar-dark bg-transparent fixed-top ";
+    }
   }
+
+
 
   return <nav className={'navbar navbar-expand-lg ' + classStr} >
     <div className="container">
-      <Link className="navbar-brand island " to='/'><h1 className="display-6">{root.alias}</h1></Link>
+      <NavLink className="navbar-brand island " to='/' ><h1 className="display-6">{root.alias}</h1></NavLink>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link to="/" className="nav-link active" aria-current="page" >
-              Home</Link>
+            <NavLink to="/" className="nav-link active" aria-current="page" >
+              Home</NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/projects" className="nav-link" >
-              Projects</Link>
+            <NavLink to="/projects" className="nav-link"  >
+              Projects</NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/travel" className="nav-link" >
-              Travel</Link>
+            <NavLink to="/travel" className="nav-link" >
+              Travel</NavLink>
           </li>
 
-        
+
           <li className="nav-item">
             <Link to="/contact" className="nav-link " tabIndex="-1" aria-disabled="false">Contact</Link>
           </li>
@@ -59,7 +62,7 @@ function Navbar() {
           </li> */}
           {/* <li className="nav-item">
             <Link to="/RishhiiiMusic" className="nav-link " tabIndex="-1" aria-disabled="false">RishhiiiMusic <i className="fa-brands fa-youtube fa-xl text-danger"></i></Link>
-          </li> */} 
+          </li> */}
 
         </ul>
 
